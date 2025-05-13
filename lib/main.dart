ff6e40fcf94df91467f088e83bc7e6dc9b82ef87
yayediop2 <yayefatoudiop2000@gmail.com>
2025-05-13 14:11:13 +0000

    feat: swipe up, down, left and right Fixed swipe gesture detection by disabling GridView scroll behavior

--- lib/main.dart
+++ lib/main.dart
@@ -48,17 +48,36 @@ class _MyHomePageState extends State<MyHomePage> {
         3 +
         randomNum.nextInt(2); // so the num generated is between 2 and 3 inclus
 
-    while (tiles.length < tileCount) {
-      int x = randomNum.nextInt(
-        4,
-      ); // so the coordinate num generated is between 0 and 4 inclus
-      int y = randomNum.nextInt(4);
+    final positions = <String>{};
 
-      if (tileAt(x, y) == null) {
-        int value = randomNum.nextDouble() < 0.9 ? 2 : 4;
+    while (tiles.length < tileCount) {
+      // so the coordinate value generated is between 0 and 4 inclus
+      int x = randomNum.nextInt(4);
+      int y = randomNum.nextInt(4);
+      String key = '$x,$y';
+
+      if (!positions.contains(key)) {
+        positions.add(key);
+        int value = randomNum.nextBool() ? 2 : 4;
         tiles.add(Tile(x: x, y: y, value: value));
       }
     }
+  }
+
+  void onSwipeLeft() {
+    print('swipe left');
+  }
+
+  void onSwipeRight() {
+    print('swipe right');
+  }
+
+  void onSwipeUp() {
+    print('swipe up');
+  }
+
+  void onSwipeDown() {
+    print('swipe down');
   }
 
   @override
@@ -71,40 +90,59 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Scaffold(
-      body: Center(
-        child: GridView.count(
-          crossAxisCount: 4,
-          children: List.generate(16, (index) {
-            int x = index % 4;
-            int y = index ~/ 4;
-            Tile? tile = tileAt(x, y);
+      body: GestureDetector(
+        onHorizontalDragEnd: (details) {
+          if (details.velocity.pixelsPerSecond.dx > 0) {
+            onSwipeRight();
+          } else {
+            onSwipeLeft();
+          }
+        },
+        onVerticalDragEnd: (details) {
+          if (details.velocity.pixelsPerSecond.dy > 0) {
+            onSwipeDown();
+          } else {
+            onSwipeUp();
+          }
+        },
+        child: Center(
+          child: GridView.count(
+            physics:
+                const NeverScrollableScrollPhysics(), // disables scrolling so swipe up ad down can work
+            shrinkWrap: true, // makes GridView size properly inside Center
+            crossAxisCount: 4,
+            children: List.generate(16, (index) {
+              int x = index % 4;
+              int y = index ~/ 4;
+              Tile? tile = tileAt(x, y);
 
-            return Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color:
-                    tile != null
-                        ? theme.colorScheme.primaryFixedDim
-                        : theme.colorScheme.secondary,
+              return Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color:
+                      tile != null
+                          ? theme.colorScheme.primaryFixedDim
+                          : theme.colorScheme.secondary,
 
-                border: Border.all(
-                  color: theme.colorScheme.onSurface,
-                  width: 3,
+                  border: Border.all(
+                    color: theme.colorScheme.onSurface,
+                    width: 3,
+                  ),
                 ),
-              ),
-              child:
-                  tile != null
-                      ? Text(
-                        tile.value.toString(),
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      )
-                      : null,
-            );
-          }),
+                child:
+                    tile != null
+                        ? Text(
+                          tile.value.toString(),
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        )
+                        : null,
+              );
+            }),
+          ),
         ),
       ),
     );
